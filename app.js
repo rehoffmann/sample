9f1e0a3821c3228a775a200249f85468076b3f31
Roman Hoffmann <hoffmann.romane@gmail.com>
2020-04-16 10:21:02 -0600

    updated navbar

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var menubar = document.getElementById("menubar");
+
+// Get the offset position of the navbar
+var sticky = menubar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    menubar.classList.add("sticky")
+  } else {
+    menubar.classList.remove("sticky");
+  }
+}
