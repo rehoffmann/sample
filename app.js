5c7fc2369d3517dca9ba80c705ab16951cdd1f17
Roman Hoffmann <hoffmann.romane@gmail.com>
2020-04-17 13:14:51 -0600

    new logo

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-window.onscroll = function() {myFunction()};
+function convertToRoman(num) {
+  return num;
+ }
+ 
+ convertToRoman(36);
 
-// Get the navbar
-var menubar = document.getElementById("menubar");
 
-// Get the offset position of the navbar
-var sticky = menubar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    menubar.classList.add("sticky")
-  } else {
-    menubar.classList.remove("sticky");
-  }
-}
